8608a5d600f13ac996d1d44cfc1b988698e1ad68
art kontreras <arturo.kontreras@gmail.com>
2014-04-06 15:52:15 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lopez::Application.config.secret_token = '5e5782575c329b965932996c6210b01e77158c53e4e6fa9529e8a0c87ef5729b051c866eab83eabfa753bc6cabc76f7ec72acce796df0bee59c56f1b5f827e71'
